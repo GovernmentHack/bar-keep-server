3f652086395ce935fa81a35ed506e4719fbba0fd
GovernmentHack <32027859+GovernmentHack@users.noreply.github.com>
2022-11-11 20:40:46 -0600

    Add sqlite db skeleton

--- bar-keep/src/index.ts
+++ bar-keep/src/index.ts
@@ -1,1 +1,11 @@
-console.log('Hello world!')
+import { Database } from 'sqlite3';
+
+const DB_LOCATION = process.env.DEV ? '../db.dev.sqlite' : '/data/db.sqlite'
+
+const db = new Database(DB_LOCATION);
+
+// Fetch a random integer between -99 and +99
+db.get(
+    'SELECT RANDOM() % 100 as result',
+    (_, res) => console.log(res)
+);
