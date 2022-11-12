7dae8497b6d2711e0689fe56c65b166978a58210
GovernmentHack <32027859+GovernmentHack@users.noreply.github.com>
2022-11-11 20:53:15 -0600

    add updated dist

--- bar-keep/rootfs/build/index.js
+++ bar-keep/rootfs/build/index.js
@@ -1,2 +1,7 @@
 "use strict";
-console.log('Hello world!');
+Object.defineProperty(exports, "__esModule", { value: true });
+const sqlite3_1 = require("sqlite3");
+const DB_LOCATION = process.env.DEV ? '../db.dev.sqlite' : '/data/db.sqlite';
+const db = new sqlite3_1.Database(DB_LOCATION);
+// Fetch a random integer between -99 and +99
+db.get('SELECT RANDOM() % 100 as result', (_, res) => console.log(res));
